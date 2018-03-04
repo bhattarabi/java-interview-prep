825e3b9fa8263de62f928daa39ac0a903aefb311
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-03 21:49:54 -0800

    finish optimized solutions for isPrime

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -5,10 +5,27 @@ public class PrimeNumber {
 	//first solution (brute force)
 	//check divisibility by all integers 2...n-1 
 	public static boolean isPrime(int n) {
-		for (int i=1; i<n; i++) {
+		if (n==1)
+			return false;
+		for (int i=2; i<n; i++) {
 			if (n%i == 0)
 				return false;
 		}
 		return true;
+	}
+	
+	//second solution
+	//only check divisiblity by integers 2...sqrt(n)
+	public static boolean isPrimeSqrt(int n) {
+		return isPrime((int)Math.sqrt(n) + 1);
+	}
+	
+	//third solution (most optimal)
+	public static boolean isPrimeMathematical(int n) {
+		if (n==1)
+			return false;
+		if (n==2)
+			return true;
+		return (n*n - 1) % 24 == 0;
 	}
 }

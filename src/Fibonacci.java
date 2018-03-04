ae7e91120923f44f6d3a09a39f07586dcf5fe5b4
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-03 20:41:07 -0800

    Finish Fibonacci solution

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,9 +1,53 @@
+//Write Java Code to compute the nth Fibonacci number
+
+import java.util.HashMap;
 
 public class Fibonacci {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//store pre-calculated values here
+	private HashMap<Integer, Integer> cache = 
+			new HashMap();
+	
+	
+	//first solution (without using the cache):
+	//calculate nth fibo number iteratively
+	public int getNthFibo(int n) {
+		int 	n1 = 1,
+				n2 = 1,
+				n3 = 1;
+		
+		for (int i=2; i<n; i++) {
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+		}
+		return n3;
 	}
-
+	
+	
+	//second solution (without using the cache):
+	//calculate nth fibo number recursively
+	public int getNthFiboRec(int n) {
+		if (n<=2)
+			return 1;
+		
+		return getNthFiboRec(n-1) + getNthFiboRec(n-2);
+	}
+	
+	
+	//third solution (using the cache):
+	//calculate nth fibo number recursively
+	public int getCachedFibo(int n) {
+		
+		if (n<=2)
+			return 1;
+		
+		if (cache.containsKey(n))
+			return cache.get(n);
+		
+		int fibo = getCachedFibo(n-1) + getCachedFibo(n-2);
+		cache.put(n, fibo);
+		
+		return fibo;
+	}
 }

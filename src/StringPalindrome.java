cfb185a9642c5215848715a8f06c099c2878fffa
binaya bhattarai <bhattaraibinayar@gmail.com>
2018-03-03 22:58:39 -0800

    add iterative solution that uses indexes

--- src/StringPalindrome.java
+++ src/StringPalindrome.java
@@ -1,7 +1,7 @@
-
 public class StringPalindrome {
 	
-	//Iterative solution
+	//Iterative solution that uses StringBuilder
+	//can also be used to return the reverse of the string 
 	public static boolean isPalindromeIter(String test) {
 		StringBuilder sb = new StringBuilder();
 		
@@ -12,6 +12,20 @@ public class StringPalindrome {
 		return test.equalsIgnoreCase(sb.toString());
 	}
 	
+	//Iterative solution that uses indexes instead of stringBuilder
+	public static boolean isPalindromeIterIndex(String test) {
+		
+		int begin = 0, end = test.length()-1;		
+		
+		while (begin<end) {
+			if (test.charAt(begin) != test.charAt(end))
+				return false;
+			
+			begin++;
+			end--;			
+		}
+		return true;
+	}
 	
 	//helper function for the recursive solution
 	private static boolean palindromeRec(String test, 
